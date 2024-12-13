03f76b035317bf6e7ada293d1b2ecffe9904bb9f
bLue <tbdblue@gmail.com>
2024-12-13 12:41:44 +0800

    fix: mongo prod config template

--- src/server/configs/mongo/mongo.config.prod.ts
+++ src/server/configs/mongo/mongo.config.prod.ts
@@ -3,7 +3,7 @@ import MongoConfig from './mongo.config';
 
 @Config(MongoConfig, { when: 'production', override: true })
 export default class MongoProdConfig extends MongoConfig {
-  public readonly url = `mongodb://${process.env.MONGO_USER}${
+  public readonly url = `mongodb://${process.env.MONGO_USER || ''}${
     process.env.MONGO_PASS ? `:${encodeURIComponent(process.env.MONGO_PASS)}` : ''
   }${process.env.MONGO_USER || process.env.MONGO_PASS ? '@' : ''}${process.env.MONGO_HOST || '127.0.0.1'}:${
     process.env.MONGO_PORT || 27017
